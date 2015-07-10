b96cd29ffc66dacd2bcbad6c3db452c08fd8e806
Jonathan Feinberg <jonathf@gmail.com>
2015-07-10 13:12:43 +0200

    dual main and total sensitivity in single run for nataf transformed input

--- chaospy/descriptives.py
+++ chaospy/descriptives.py
@@ -912,6 +912,68 @@ Returns:
 
     return out
 
+def Sens_nataf(order, dist, samples, vals, **kws):
+    """
+Variance-based decomposition with dependent varibles thorugh the Nataf
+distribution.
+
+Main and total order sensitivity indices
+
+Args:
+    order (int): polynomial order used `orth_ttr`.
+    dist (Copula): Assumed to be Nataf with independent components
+    samples (array_like): Samples used for evaluation (typically generated from `dist`.)
+    vals (array_like): Evaluations of the model for given samples.
+
+Kwrds: Passed to `E`, `E_cond` and `Var` as part of the method.
+
+Returns:
+        np.ndarray: Sensitivity indices with `shape==(2,len(dist),)+vals.shape[1:]`.
+        First component is main and second is total.
+    """
+
+    assert dist.__class__.__name__ == "Copula"
+    trans = dist.prm["trans"]
+    assert trans.__class__.__name__ == "nataf"
+    vals = np.array(vals)
+
+    cov = trans.prm["C"]
+    cov = np.dot(cov, cov.T)
+
+    marginal = dist.prm["dist"]
+    dim = len(dist)
+
+    orth = ort.orth_ttr(order, marginal, sort="GR")
+
+    r = range(dim)
+
+    index0 = [0] + [1]*(dim-1)
+    index1 = [1] + [0]*(dim-1)
+
+    nataf = di.Nataf(marginal, cov, r)
+    samples_ = marginal.inv( nataf.fwd( samples ) )
+    poly, coeffs = co.fit_regression(orth, samples_, vals, retall=1)
+
+    V = Var(poly, marginal, **kws)
+
+    out = np.zeros((2, dim,) + poly.shape)
+    out[0,0] = (V-Var(E_cond(poly, index0, marginal, **kws), marginal, **kws))/(V+(V==0))**(V!=0)
+    out[1,0] = Var(E_cond(poly, index1, marginal, **kws), marginal, **kws)/(V+(V==0))*(V!=0)
+
+
+    for i in xrange(1, dim):
+
+        r = r[1:] + r[:1]
+        index0 = index0[-1:] + index0[:-1]
+
+        nataf = di.Nataf(marginal, cov, r)
+        samples_ = marginal.inv( nataf.fwd( samples ) )
+        poly, coeffs = co.fit_regression(orth, samples_, vals, retall=1)
+
+        out[0,i] = (V-Var(E_cond(poly, index0, marginal, **kws), marginal, **kws))/(V+(V==0))*(V!=0)
+        out[1,i] = Var(E_cond(poly, index1, marginal, **kws), marginal, **kws)/(V+(V==0))*(V!=0)
+
+    return out[::-1]
 
 import numpy as np
 import poly as po
