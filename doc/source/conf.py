0bf2af572b33e3a14b7adaf359eb8a9b02597891
Jonathan Feinberg <jonathf@gmail.com>
2016-10-03 11:44:07 +0200

    Bugfixes and documentation
    * Documentation on installation, testing, troubleshooting etc.
    * Adaptive to add code-coverage support
    * Fixup for badges
    * Tick version
    * solves #6 and #13

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -184,7 +184,11 @@ html_static_path = ['_static']
 
 # Custom sidebar templates, maps document names to template names.
 #
-# html_sidebars = {}
+html_sidebars = {
+    '**': [
+        'globaltoc.html', 'relations.html', 'sourcelink.html', 'searchbox.html'
+    ],
+}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
