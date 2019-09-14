5c620c5a8ecf197cf3b6ba7cc8daf549b41acfba
Jonathan Feinberg <jonathf@gmail.com>
2019-09-14 13:29:51 +0200

    conditional_experiment

--- chaospy/descriptives/conditional.py
+++ chaospy/descriptives/conditional.py
@@ -37,32 +37,59 @@ def E_cond(poly, freeze, dist, **kws):
         >>> print(chaospy.E_cond(poly, [x, y], dist))
         [1 x y 10*x*y]
         >>> print(chaospy.E_cond(poly, [0, 0], dist))
-        [1.0 1.0 0.0 0.0]
+        [1. 1. 0. 0.]
+        >>> print(chaospy.E_cond(poly, numpy.eye(2), dist))
+        [[1.0 x 0.0 0.0]
+         [1.0 1.0 y 10.0*y]]
     """
+    if isinstance(poly, chaospy.Dist):
+        dist, poly = poly, numpoly.symbols("q:%d" % len(poly))
+    poly = numpoly.polynomial(poly)
+    poly, _ = numpoly.align_indeterminants(poly, numpoly.symbols(
+        poly.names+tuple("zzz%d__" % idx
+                         for idx in range(len(dist)-len(poly.names)))))
+
+    assert not chaospy.distributions.evaluation.get_dependencies(*dist), dist
+
     freeze = numpoly.polynomial(freeze)
     if freeze.isconstant():
-        freeze = freeze.astype(bool)
+        freeze = freeze.toarray().astype(bool)
     else:
         poly, freeze = numpoly.align_exponents(poly, freeze)
         freeze = numpy.isin(poly.keys, freeze.keys)
-    if numpy.all(freeze):
-        return poly.copy()
 
-    poly_indep, poly_dep = _split_out_conditions(poly, freeze)
-    dist = [dist_ for dist_, keep in zip(dist, freeze) if not keep]
-
-    out = chaospy.E(poly_dep, chaospy.J(*dist))*poly_indep
-    out, _ = numpoly.align_indeterminants(out, poly)
+    cache = {}
+    if len(freeze.shape) == 1:
+        out = _E_cond(poly, freeze, dist, cache, **kws)
+    else:
+        out = numpoly.concatenate([
+            _E_cond(poly, freeze_, dist, cache, **kws)[numpy.newaxis]
+            for freeze_ in freeze
+        ])
+    if out.isconstant():
+        out = out.toarray()
     return out
 
 
-def _split_out_conditions(poly, freeze):
+def _E_cond(poly, freeze, dist, cache, **kws):
+    """Backend for conditional expected value."""
+    assert len(poly.names) == len(freeze) == len(dist)
+    if numpy.all(freeze):
+        return poly.copy()
     poly1 = poly(**{str(var): (var if keep else 1)
                     for var, keep in zip(poly.indeterminants, freeze)})
-
     poly2 = poly(**{str(var): (var if not keep else 1)
                     for var, keep in zip(poly.indeterminants, freeze)})
-    # reset non-zero coefficients to 1 to not duplciating them:
+    assert len(poly.names) == len(poly2.names)
+    # reset non-zero coefficients to 1 so not to duplicate them:
     for key in poly2.keys:
-        poly2[key][numpy.nonzero(poly2[key])] = 1
-    return poly1, poly2
+        poly2[poly2 != 0] = 1
+
+    out = numpoly.sum([
+        chaospy.distributions.evaluation.evaluate_moment(
+            dist, (exponent*~freeze), cache, **kws)*coefficient
+        for exponent, coefficient in zip(poly2.exponents, poly2.coefficients)
+    ], axis=0)*poly1
+
+    out, _ = numpoly.align_indeterminants(out, poly)
+    return out
