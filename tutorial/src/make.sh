642398c2760807de6e0e9c89ca13ef97bbf32e68
Jonathan Feinberg <jonathf@gmail.com>
2020-02-24 17:00:10 +0100

    (Temporarily) clean out old stuff

--- tutorial/src/make.sh
+++
@@ -1,7 +0,0 @@
-pdflatex tutorial_1.tex
-pdflatex tutorial_2.tex
-pdflatex tutorial_3.tex
-pdflatex tutorial_4.tex
-pdflatex exercise.tex
-pdflatex solutions.tex
-pdflatex presentation_siamUQ_2016.tex
