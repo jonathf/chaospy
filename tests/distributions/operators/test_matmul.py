2755b29b8dd35976812834edc40de4e36544e7fe
Jonathan Feinberg <jonathf@gmail.com>
2020-04-01 11:09:41 +0200

    py27 fix

--- tests/distributions/operators/test_matmul.py
+++ tests/distributions/operators/test_matmul.py
@@ -1,3 +1,9 @@
+"""
+Testing of the matmul operator '@' for distributions.
+
+For py27 reasons, the dunder-methods '__matmul__' and '__rmatmul__' are used
+instead of the literal '@' operator.
+"""
 from pytest import raises
 import numpy
 
@@ -13,69 +19,69 @@ EPS = 1E-1
 
 def test_dist_matmul_illigals():
     with raises(ValueError):
-        UNIVARIATE @ 4
+        UNIVARIATE.__matmul__(4)
     with raises(ValueError):
-        4 @ UNIVARIATE
+        UNIVARIATE.__rmatmul__(4)
     with raises(ValueError):
-        MULTIVARIATE @ 4
+        MULTIVARIATE.__matmul__(4)
     with raises(ValueError):
-        4 @ MULTIVARIATE
+        MULTIVARIATE.__rmatmul__(4)
     with raises(ValueError):
-        UNIVARIATE @ [2, 3]
+        UNIVARIATE.__matmul__([2, 3])
     with raises(ValueError):
-        [2, 3] @ UNIVARIATE
+        UNIVARIATE.__rmatmul__([2, 3])
     with raises(ValueError):
         chaospy.Matmul([3, 4], [2, 3])
 
 
 def test_dist_matmul_lower():
-    assert all((MULTIVARIATE @ [2, 3]).lower == [2, 6])
-    assert all(([2, 3] @ MULTIVARIATE).lower == [2, 6])
-    assert all((MULTIVARIATE @ [[1, 1], [0, 1]]).lower == [1, 3])
-    assert all(([[1, 1], [0, 1]] @ MULTIVARIATE).lower == [3, 2])
+    assert all((MULTIVARIATE.__matmul__([2, 3])).lower == [2, 6])
+    assert all((MULTIVARIATE.__rmatmul__([2, 3])).lower == [2, 6])
+    assert all((MULTIVARIATE.__matmul__([[1, 1], [0, 1]])).lower == [1, 3])
+    assert all((MULTIVARIATE.__rmatmul__([[1, 1], [0, 1]])).lower == [3, 2])
 
 
 def test_dist_matmul_upper():
-    assert all((MULTIVARIATE @ [2, 3]).upper == [4, 12])
-    assert all(([2, 3] @ MULTIVARIATE).upper == [4, 12])
-    assert all((MULTIVARIATE @ [[1, 1], [0, 1]]).upper == [2, 6])
-    assert all(([[1, 1], [0, 1]] @ MULTIVARIATE).upper == [6, 4])
+    assert all((MULTIVARIATE.__matmul__([2, 3])).upper == [4, 12])
+    assert all((MULTIVARIATE.__rmatmul__([2, 3])).upper == [4, 12])
+    assert all((MULTIVARIATE.__matmul__([[1, 1], [0, 1]])).upper == [2, 6])
+    assert all((MULTIVARIATE.__rmatmul__([[1, 1], [0, 1]])).upper == [6, 4])
 
 
 def test_dist_matmul_forward():
-    assert numpy.allclose((MULTIVARIATE @ [2, 3]).fwd([[2, 3, 4], [12, 9, 6]]),
+    assert numpy.allclose((MULTIVARIATE.__matmul__([2, 3])).fwd([[2, 3, 4], [12, 9, 6]]),
                           [[0, 0.5, 1], [1, 0.5, 0]])
-    assert numpy.allclose(([2, 3] @ MULTIVARIATE).fwd([[2, 3, 4], [12, 9, 6]]),
+    assert numpy.allclose((MULTIVARIATE.__rmatmul__([2, 3])).fwd([[2, 3, 4], [12, 9, 6]]),
                           [[0, 0.5, 1], [1, 0.5, 0]])
-    assert numpy.allclose((MULTIVARIATE @ [[1, 1], [0, 1]]).fwd([[1, 1.5, 2], [6, 4.5, 3]]),
+    assert numpy.allclose((MULTIVARIATE.__matmul__([[1, 1], [0, 1]])).fwd([[1, 1.5, 2], [6, 4.5, 3]]),
                           [[0, 0.5, 1], [1, 0.5, 0]])
-    assert numpy.allclose(([[1, 1], [0, 1]] @ MULTIVARIATE).fwd([[3, 4.5, 6], [4, 3, 2]]),
+    assert numpy.allclose((MULTIVARIATE.__rmatmul__([[1, 1], [0, 1]])).fwd([[3, 4.5, 6], [4, 3, 2]]),
                           [[0, 0.5, 1], [1, 0.5, 0]])
 
 
 def test_dist_matmul_inverse():
-    assert numpy.allclose((MULTIVARIATE @ [2, 3]).inv([[0, 0.5, 1], [1, 0.5, 0]]),
+    assert numpy.allclose((MULTIVARIATE.__matmul__([2, 3])).inv([[0, 0.5, 1], [1, 0.5, 0]]),
                           [[2, 3, 4], [12, 9, 6]])
-    assert numpy.allclose(([2, 3] @ MULTIVARIATE).inv([[0, 0.5, 1], [1, 0.5, 0]]),
+    assert numpy.allclose((MULTIVARIATE.__rmatmul__([2, 3])).inv([[0, 0.5, 1], [1, 0.5, 0]]),
                           [[2, 3, 4], [12, 9, 6]])
-    assert numpy.allclose((MULTIVARIATE @ [[1, 1], [0, 1]]).inv([[0, 0.5, 1], [0, 0.5, 1]]),
+    assert numpy.allclose((MULTIVARIATE.__matmul__([[1, 1], [0, 1]])).inv([[0, 0.5, 1], [0, 0.5, 1]]),
                           [[1, 1.5, 2], [3, 4.5, 6]])
-    assert numpy.allclose(([[1, 1], [0, 1]] @ MULTIVARIATE).inv([[0, 0.5, 1], [0, 0.5, 1]]),
+    assert numpy.allclose((MULTIVARIATE.__rmatmul__([[1, 1], [0, 1]])).inv([[0, 0.5, 1], [0, 0.5, 1]]),
                           [[3, 4.5, 6], [2, 3, 4]])
 
 
 def test_dist_matmul_density():
-    assert numpy.allclose((MULTIVARIATE @ [2, 3]).pdf([[2-EPS, 2+EPS, 4-EPS, 4+EPS],
+    assert numpy.allclose((MULTIVARIATE.__matmul__([2, 3])).pdf([[2-EPS, 2+EPS, 4-EPS, 4+EPS],
                                                        [6-EPS, 6+EPS, 12-EPS, 12+EPS]]),
                           [[0, 1/12, 1/12, 0]])
-    assert numpy.allclose(([2, 3] @ MULTIVARIATE).pdf([[2-EPS, 2+EPS, 4-EPS, 4+EPS],
+    assert numpy.allclose((MULTIVARIATE.__rmatmul__([2, 3])).pdf([[2-EPS, 2+EPS, 4-EPS, 4+EPS],
                                                        [6-EPS, 6+EPS, 12-EPS, 12+EPS]]),
                           [[0, 1/12, 1/12, 0]])
-    assert numpy.allclose((MULTIVARIATE @ [[1, 1], [0, 1]]).pdf([[1, 1, 1, 1, 2, 2, 2, 2],
+    assert numpy.allclose((MULTIVARIATE.__matmul__([[1, 1], [0, 1]])).pdf([[1, 1, 1, 1, 2, 2, 2, 2],
                                                                  [3-EPS, 3+EPS, 5-EPS, 5+EPS,
                                                                   4-EPS, 4+EPS, 6-EPS, 6+EPS]]),
                           [0, 0.5, 0.5, 0, 0, 0.5, 0.5, 0])
-    assert numpy.allclose(([[1, 1], [0, 1]] @ MULTIVARIATE).pdf([[3-EPS, 3+EPS, 5-EPS, 5+EPS,
+    assert numpy.allclose((MULTIVARIATE.__rmatmul__([[1, 1], [0, 1]])).pdf([[3-EPS, 3+EPS, 5-EPS, 5+EPS,
                                                                   4-EPS, 4+EPS, 6-EPS, 6+EPS],
                                                                  [2, 2, 4, 4, 2, 2, 4, 4]]),
                           [0, 0.25, 0, 0.25, 0.25, 0, 0.25, 0])
@@ -83,12 +89,12 @@ def test_dist_matmul_density():
 
 def test_dist_matmul_moment():
     pass
-    # assert all((MULTIVARIATE @ [2, 3]).mom([[1, 0, 1], [0, 1, 1]]) == [0, 0])
-    # assert all(([2, 3] @ MULTIVARIATE).mom([[1, 0, 1], [0, 1, 1]]) == [0, 0])
-    # assert all((MULTIVARIATE @ [[1, 1], [0, 1]]).mom([[1, 0, 1], [0, 1, 1]]) == [0, 0])
-    # assert all(([[1, 1], [0, 1]] @ MULTIVARIATE).mom([[1, 0, 1], [0, 1, 1]]) == [0, 0])
+    # assert all((MULTIVARIATE.__matmul__([2, 3])).mom([[1, 0, 1], [0, 1, 1]]) == [0, 0])
+    # assert all((MULTIVARIATE.__rmatmul__([2, 3])).mom([[1, 0, 1], [0, 1, 1]]) == [0, 0])
+    # assert all((MULTIVARIATE.__matmul__([[1, 1], [0, 1]])).mom([[1, 0, 1], [0, 1, 1]]) == [0, 0])
+    # assert all((MULTIVARIATE.__rmatmul__([[1, 1], [0, 1]])).mom([[1, 0, 1], [0, 1, 1]]) == [0, 0])
 
 
 def test_dist_matmul_recurrence():
     with raises(chaospy.DependencyError):
-        _ = (MULTIVARIATE @ [2, 3]).ttr([1, 1])
+        _ = (MULTIVARIATE.__matmul__([2, 3])).ttr([1, 1])
