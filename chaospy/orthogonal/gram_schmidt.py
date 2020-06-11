1124d8838bc5d044754883a88f41a25e7f798e23
Jonathan Feinberg <jonathf@gmail.com>
2020-06-11 11:06:41 +0200

    update orthogonal functions

--- chaospy/orthogonal/gram_schmidt.py
+++ chaospy/orthogonal/gram_schmidt.py
@@ -10,7 +10,7 @@ import chaospy
 
 
 def orth_gs(order, dist, normed=False, graded=True, reverse=True,
-            cross_truncation=1., sort=None, **kws):
+            retall=False, cross_truncation=1., sort=None, **kws):
     """
     Gram-Schmidt process for generating orthogonal polynomials.
 
@@ -31,6 +31,9 @@ def orth_gs(order, dist, normed=False, graded=True, reverse=True,
         reverse (bool):
             Reverse lexicographical sorting meaning that ``q0*q1**3`` is
             considered bigger than ``q0**3*q1``, instead of the opposite.
+        retall (bool):
+            If true return numerical stabilized norms as well. Roughly the same
+            as ``cp.E(orth**2, dist)``.
         cross_truncation (float):
             Use hyperbolic cross truncation scheme to reduce the number of
             terms in expansion.
@@ -40,9 +43,16 @@ def orth_gs(order, dist, normed=False, graded=True, reverse=True,
             The orthogonal polynomial expansion.
 
     Examples:
-        >>> Z = chaospy.J(chaospy.Normal(), chaospy.Normal())
-        >>> chaospy.orth_gs(2, Z).round(4)
+        >>> distribution = chaospy.J(chaospy.Normal(), chaospy.Normal())
+        >>> polynomials, norms = chaospy.orth_gs(2, distribution, retall=True)
+        >>> polynomials.round(10)
         polynomial([1.0, q1, q0, q1**2-1.0, q0*q1, q0**2-1.0])
+        >>> norms.round(10)
+        array([1., 1., 1., 2., 1., 2.])
+        >>> polynomials = chaospy.orth_gs(2, distribution, normed=True)
+        >>> polynomials.round(3)
+        polynomial([1.0, q1, q0, 0.707*q1**2-0.707, q0*q1, 0.707*q0**2-0.707])
+
     """
     logger = logging.getLogger(__name__)
     dim = len(dist)
@@ -59,41 +69,25 @@ def orth_gs(order, dist, normed=False, graded=True, reverse=True,
 
     polynomials = [basis[0]]
 
-    if normed:
-        for idx in range(1, len(basis)):
+    norms = [1.]
+    for idx in range(1, len(basis)):
 
-            # orthogonalize polynomial:
-            for idy in range(idx):
-                orth = chaospy.descriptives.E(
-                    basis[idx]*polynomials[idy], dist, **kws)
-                basis[idx] = basis[idx] - polynomials[idy]*orth
+        # orthogonalize polynomial:
+        for idy in range(idx):
+            orth = chaospy.E(basis[idx]*polynomials[idy], dist, **kws)
+            basis[idx] = basis[idx]-polynomials[idy]*orth/norms[idy]
 
-            # normalize:
-            norms = chaospy.descriptives.E(polynomials[-1]**2, dist, **kws)
-            if norms <= 0:
-                logger.warning("Warning: Polynomial cutoff at term %d", idx)
-                break
-            basis[idx] = basis[idx] / numpy.sqrt(norms)
+        norms_ = chaospy.E(basis[idx]**2, dist, **kws)
+        if norms_ <= 0:
+            logger.warning("Warning: Polynomial cutoff at term %d", idx)
+            break
 
-            polynomials.append(basis[idx])
+        norms.append(1. if normed else norms_)
+        basis[idx] = basis[idx]/numpy.sqrt(norms_) if normed else basis[idx]
+        polynomials.append(basis[idx])
 
-    else:
-
-        norms = [1.]
-        for idx in range(1, len(basis)):
-
-            # orthogonalize polynomial:
-            for idy in range(idx):
-                orth = chaospy.descriptives.E(
-                    basis[idx]*polynomials[idy], dist, **kws)
-                basis[idx] = basis[idx] - polynomials[idy] * orth / norms[idy]
-
-            norms.append(
-                chaospy.descriptives.E(polynomials[-1]**2, dist, **kws))
-            if norms[-1] <= 0:
-                logger.warning("Warning: Polynomial cutoff at term %d", idx)
-                break
-
-            polynomials.append(basis[idx])
-
-    return chaospy.poly.polynomial(polynomials).reshape((len(polynomials),))
+    polynomials = chaospy.polynomial(polynomials).flatten()
+    if retall:
+        norms = numpy.array(norms)
+        return polynomials, norms
+    return polynomials
